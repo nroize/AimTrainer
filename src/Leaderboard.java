f0bb50a2b2f453ffb3b33bbce59c622a14ac01f1
JonahGladstone <73909833+JonahGladstone@users.noreply.github.com>
2020-11-04 15:24:35 -0500

    Started the constructor and private convert method

--- src/Leaderboard.java
+++ src/Leaderboard.java
@@ -1,22 +1,44 @@
+import java.io.*;
 import java.util.HashMap;
 import java.awt.print.Printable;
-import java.io.FileWriter;
-import java.io.PrintWriter;
+
 /*
     TODO:
         - Make Leaderboard constructor take in directory, make add to variable
 */
 public class Leaderboard {
     HashMap<String, Integer> lb;
-    public Leaderboard(String dir) {
+
+    public Leaderboard(String cons) {
+
+
+        //FileWriter fw= new FileWriter("leaderboard.csv");
+        //   PrintWriter pw= new PrintWriter(fw);
+
+
+        //pw.close();
+        lb = convert("Leaderboard.csv");
     }
-    public static void main(String[] args) throws Exception{
-        FileWriter fw= new FileWriter("leaderboard.csv");
-        PrintWriter pw= new PrintWriter(fw);
 
+    private HashMap convert(String fileName) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader("Leaderboard.csv"));
+        String line = null;
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
 
-        
-        pw.close();
-
+        while ((line = br.readLine()) != null) {
+            String str[] = line.split(",");
+            map.put(str[0],(int)str[1]);
+        }
     }
 }
+
+
+/*
+5 methods
+- constructor- blank, calls private method
+- private method- convert csv to hash map
+- get leaderboard, get inputs returns maps
+- write to leaderboard, takes 2 inputs, adds a line with that information
+- sorting the hash map
+
+ */
